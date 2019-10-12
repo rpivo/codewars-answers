f27af87240c4e1a4351f35da3306368a6a93014d
Ryan Pivovar <ryanpivovar@gmail.com>
2019-10-12 00:48:52 -0700

    solve compare-with-margin

---
+++ compare-within-margin.js
@@ -0,0 +1,12 @@
+const closeCompare = (a, b, margin) => {
+  if (!margin) margin = 0;
+  if (a > b) return (a - b) > margin ? 1 : 0;
+  if (b > a) return (b - a) > margin ? -1 : 0;
+  return 0;
+};
+
+a = -3;
+b = 5;
+margin = 3;
+
+console.log(close_compare(a, b, margin));
